c312318960efcd3307ba9a48d11f708f84eb0479
Jacob Mastel <mastelj@mastelj.org>
2013-09-10 09:26:00 -0700

    initial github load

---
+++ stikked.py
@@ -0,0 +1,41 @@
+import sublime, sublime_plugin
+import os, urllib, re
+
+class stikked(sublime_plugin.TextCommand):
+    #Run when plugin starts
+    def run(self, edit):
+
+        #Load the user's settings
+        s = sublime.load_settings('Stikked.sublime-settings')
+        author = s.get("author")
+        url = s.get("url")
+
+        #Get the censored view of our current file
+        text = self.get_text()
+
+        #Encode and send out data
+        data = urllib.urlencode( {"title":self.cur_file(), "text":text, "name":author, "lang":self.cur_syntax()} )
+        u = urllib.urlopen(url, data)
+
+        #Set the url to the clipboard
+        sublime.set_clipboard(u.read())
+
+    #Get the syntax of the current file
+    def cur_syntax(self):
+        syntax = self.view.settings().get('syntax')
+        syntax = syntax.split("/", 2)
+        return syntax[1]
+
+    #Return the name of the file without the path
+    def cur_file(self):
+        return os.path.basename(self.view.file_name())
+
+    def get_text(self):
+        text = self.view.substr(sublime.Region(0, self.view.size()))
+        to_kill = sublime.load_settings('Stikked.sublime-settings').get("kill")
+        to_rep  = sublime.load_settings('Stikked.sublime-settings').get("replace")
+        
+        for rage in to_kill:
+            text = re.sub(rage[0], rage[1], text)
+        
+        return text

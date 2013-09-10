6a709b1208c0a8e5d29fd4294debb603201aa458
Jacob Mastel <mastelj@mastelj.org>
2013-09-10 16:24:43 -0700

    fiddling with settings

--- stikked.py
+++ stikked.py
@@ -2,46 +2,55 @@ import sublime, sublime_plugin
 import os, urllib, re
 
 class stikked(sublime_plugin.TextCommand):
-    #Run when plugin starts
-    def run(self, edit, **args):
+	#Run when plugin starts
+	def run(self, edit, **args):
 
-        #Load the user's settings
-        s = sublime.load_settings('Stikked.sublime-settings')
-        author = s.get("author")
-        url = s.get("url")
+		#Load the user's settings
+		s = sublime.load_settings('Stikked.sublime-settings')
+		author = s.get("author")
+		url = s.get("url")
 
-        #Get the censored view of our current file
-        text = self.get_text()
+		#Get the censored view of our current file
+		text = self.get_text()
 
-        #decide if private
-        if args['private'] == 'true'
-            private = 1
-        else
-            private = 0
+		#decide if private
+		if args['private'] == 'true':
+			private = 1
+		else:
+			private = 0
+		
+		if args['reply'] == 'true':
+			reply = self.view.window().show_input_panel("Reply to:", "", self.user_input, None, None)
+			reply = reply.substr(0)
+			data = urllib.urlencode( {"title":self.cur_file(), "text":text, "name":author, "lang":self.cur_syntax(), "private":private, "reply":reply } )
+		else:
+			data = urllib.urlencode( {"title":self.cur_file(), "text":text, "name":author, "lang":self.cur_syntax(), "private":private } )
+			
+		#Encode and send out data
+		u = urllib.urlopen(url, data)
 
-        #Encode and send out data
-        data = urllib.urlencode( {"title":self.cur_file(), "text":text, "name":author, "lang":self.cur_syntax(), "private":private } )
-        u = urllib.urlopen(url, data)
+		#Set the url to the clipboard
+		sublime.set_clipboard(u.read())
 
-        #Set the url to the clipboard
-        sublime.set_clipboard(u.read())
+	#Get the syntax of the current file
+	def cur_syntax(self):
+		syntax = self.view.settings().get('syntax')
+		syntax = syntax.split("/", 2)
+		return syntax[1]
 
-    #Get the syntax of the current file
-    def cur_syntax(self):
-        syntax = self.view.settings().get('syntax')
-        syntax = syntax.split("/", 2)
-        return syntax[1]
+	#Return the name of the file without the path
+	def cur_file(self):
+		return os.path.basename(self.view.file_name())
 
-    #Return the name of the file without the path
-    def cur_file(self):
-        return os.path.basename(self.view.file_name())
+	def get_text(self):
+		text = self.view.substr(sublime.Region(0, self.view.size()))
+		to_kill = sublime.load_settings('Stikked.sublime-settings').get("kill")
+		to_rep  = sublime.load_settings('Stikked.sublime-settings').get("replace")
+		
+		for rage in to_kill:
+			text = re.sub(rage[0], rage[1], text)
+		
+		return text
 
-    def get_text(self):
-        text = self.view.substr(sublime.Region(0, self.view.size()))
-        to_kill = sublime.load_settings('Stikked.sublime-settings').get("kill")
-        to_rep  = sublime.load_settings('Stikked.sublime-settings').get("replace")
-        
-        for rage in to_kill:
-            text = re.sub(rage[0], rage[1], text)
-        
-        return text
+	def user_input(self, input):
+		return input
